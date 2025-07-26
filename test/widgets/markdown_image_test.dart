47bf024b43e9c7330bdcdc243dc87bc5bbd6d9d2
Hooshyar <hooseyr@gmail.com>
2025-07-26 23:24:16 +0400

    Fix remaining flutter_markdown import in test file

--- test/widgets/markdown_image_test.dart
+++ test/widgets/markdown_image_test.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_gen_ai_chat_ui/flutter_gen_ai_chat_ui.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:flutter_markdown/flutter_markdown.dart';
+import 'package:flutter_markdown_plus/flutter_markdown_plus.dart';
 
 void main() {
   group('Markdown Image Tap Tests', () {
